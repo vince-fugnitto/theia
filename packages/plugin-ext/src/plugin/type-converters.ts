e0b33c93306f119d2d88dec21878adb660f99fff
Miro Spönemann <miro.spoenemann@typefox.io>
2020-11-16 13:56:22 +0000

    #8755: Apply problem matchers specified by task provider
    Signed-off-by: Miro Spönemann <miro.spoenemann@typefox.io>

--- packages/plugin-ext/src/plugin/type-converters.ts
+++ packages/plugin-ext/src/plugin/type-converters.ts
@@ -697,6 +697,10 @@ export function toWorkspaceFolder(folder: model.WorkspaceFolder): theia.Workspac
     };
 }
 
+interface ExtendedTask extends theia.Task2 {
+    hasProblemMatchers: boolean;
+}
+
 export function fromTask(task: theia.Task): TaskDto | undefined {
     if (!task) {
         return undefined;
@@ -705,8 +709,11 @@ export function fromTask(task: theia.Task): TaskDto | undefined {
     const taskDto = {} as TaskDto;
     taskDto.label = task.name;
     taskDto.source = task.source;
+    if ((task as ExtendedTask).hasProblemMatchers) {
+        taskDto.problemMatcher = task.problemMatchers;
+    }
     if ('detail' in task) {
-        taskDto.detail = (task as theia.Task2).detail;
+        taskDto.detail = (task as ExtendedTask).detail;
     }
     if (typeof task.scope === 'object') {
         taskDto.scope = task.scope.uri.toString();
@@ -748,7 +755,7 @@ export function toTask(taskDto: TaskDto): theia.Task {
         throw new Error('Task should be provided for converting');
     }
 
-    const { type, label, source, scope, detail, command, args, options, windows, ...properties } = taskDto;
+    const { type, label, source, scope, problemMatcher, detail, command, args, options, windows, ...properties } = taskDto;
     const result = {} as theia.Task;
     result.name = label;
     result.source = source;
